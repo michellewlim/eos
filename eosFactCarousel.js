e11991a427183c02a8ee9464170a28722395dcc2
katelynsyu <41168140+katelynsyu@users.noreply.github.com>
2018-07-27 14:51:49 -0700

    Update eosFactCarousel.js

--- eosFactCarousel.js
+++ eosFactCarousel.js
@@ -11,7 +11,7 @@ showSlides(slideIndex = n);
 
 function showSlides(n) {
 var i;
-var slides = document.getElementsByClassName("mySlides");
+var slides = document.getElementsByClassName("factSlides");
 var dots = document.getElementsByClassName("dot");
 if (n > slides.length) {slideIndex = 1}
 if (n < 1) {slideIndex = slides.length}
