ec48f66a3ccc79564f854dd173ec6383a12b0c0e
katelynsyu <41168140+katelynsyu@users.noreply.github.com>
2018-07-30 11:05:59 -0700

    Update eosFactCarousel.js

--- eosFactCarousel.js
+++ eosFactCarousel.js
@@ -5,3 +5,32 @@ function openModal() {
 function closeModal() {
   document.getElementById('myModal').style.display = "none";
 }
+
+var slideFIndex = 1;
+showFSlides(slideFIndex);
+
+function plusFSlides(m) {
+  showFSlides(slideFIndex += m);
+}
+
+function currentFSlide(n) {
+  showFSlides(slideFIndex = m);
+}
+
+function showSlides(m) {
+  var t;
+  var slides = document.getElementsByClassName("factSlides");
+  var circ = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (m > slides.length) {slideFIndex = 1}
+  if (m < 1) {slideFIndex = slides.length}
+  for (t = 0; t < slides.length; t++) {
+      slides[t].style.display = "none";
+  }
+  for (t = 0; t < circ.length; t++) {
+      circ[t].className = circ[t].className.replace(" active", "");
+  }
+  slides[slideFIndex-1].style.display = "block";
+  circ[slideIndex-1].className += " active";
+  captionText.innerHTML = circ[slideFIndex-1].alt;
+}
